424c19ba69c9a96106356ed1fd37de282268605c
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2023-07-20 06:37:29 -0700

    Updating event_manager.rb

--- event_manager.rb
+++ event_manager.rb
@@ -1,9 +1,10 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 def legislators_by_zipcode(zip)
@@ -21,7 +22,22 @@ def legislators_by_zipcode(zip)
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def clean_phone_number(phone_number)
+  digits_only = phone_number.to_s.gsub(/\D/, '')
+
+  if digits_only.length == 10
+    # Good 10-digit number
+    digits_only
+  elsif digits_only.length == 11 && digits_only[0] == '1'
+    # Trim the leading '1' for an 11-digit number
+    digits_only[1..-1]
+  else
+    # Assume bad number for all other cases
+    nil
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -40,7 +56,7 @@ contents = CSV.open(
 )
 
 template_letter = File.read('/Users/arieltyson/Desktop/event_manager/Event-Manager/form_letter.erb')
-erb_template = ERB.new template_letter
+erb_template = ERB.new(template_letter)
 
 contents.each do |row|
   id = row[0]
@@ -48,7 +64,25 @@ contents.each do |row|
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+  # Phone number validation
+  phone_number = clean_phone_number(row[:phone])
 
-  save_thank_you_letter(id,form_letter)
+  unless phone_number.nil?
+    # If phone number is valid, you can use it here
+    puts "Valid phone number: #{phone_number}"
+  else
+    puts "Invalid phone number: #{row[:phone]}"
+  end
+
+  # Time and day targeting
+  registration_date_time = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M')
+  registration_hour = registration_date_time.hour
+  registration_day_of_week = registration_date_time.strftime('%A')
+
+  puts "Registration Hour: #{registration_hour}"
+  puts "Day of the Week: #{registration_day_of_week}"
+
+  # The rest of your code for generating the form letter and saving it goes here...
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 end
